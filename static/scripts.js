4091d16d21046117f735cff1208d83a641a40a30
Johan <Johan.elfing@hiq.se>
2024-10-29 15:39:19 +0100

    fucking hell update shit fucker shit

--- static/scripts.js
+++ static/scripts.js
@@ -1,11 +1,11 @@
 window.onload = function() {
     if (!localStorage.getItem("popupShown")) {
-        document.getElementById("declinePopup").style.display = "flex";
+        document.getElementById("popupModal").style.display = "flex";
     }
 };
 
 function closePopup() {
-    document.getElementById("declinePopup").style.display = "none";
+    document.getElementById("popupModal").style.display = "none";
     localStorage.setItem("popupShown", "true");
 }
 
@@ -16,3 +16,27 @@ function acceptAll() {
 function declineAll() {
     closePopup();
 }
+
+let currentIndex = 0;
+const images = document.querySelectorAll(".image");
+
+function showSlide(index) {
+    images.forEach((img, i) => {
+        img.classList.remove("show"); 
+        if (i === index) {
+            img.classList.add("show"); 
+        }
+    });
+}
+
+function changeSlide(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
